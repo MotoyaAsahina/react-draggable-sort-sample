bf423b02300f27e0c8abba0d5234d78c5bf65221
MotoyaAsahina <motoyaasahina@icloud.com>
2024-07-31 00:37:16 +0900

    refactoring Panel

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -6,6 +6,9 @@ type PanelProps = {
   items: string[]
 }
 
+const getItemsSlice = (list: HTMLDivElement, start: number, end?: number) =>
+  Array.from(list.children).slice(start, end ?? list.childElementCount)
+
 const animate = (item: HTMLElement, direction: 'up' | 'down') => {
   item.animate(
     [
@@ -20,74 +23,84 @@ const animate = (item: HTMLElement, direction: 'up' | 'down') => {
   )
 }
 
+const moveItems = (list: HTMLDivElement, prevIndex: number, newIndex: number) => {
+  const item = list.children[prevIndex]
+  const target = list.children[newIndex]
+
+  if (prevIndex < newIndex) {
+    target.after(item)
+
+    getItemsSlice(list, prevIndex, newIndex).forEach((item) => {
+      animate(item as HTMLElement, 'up')
+    })
+    animate(item as HTMLElement, 'down')
+  } else {
+    target.before(item)
+
+    getItemsSlice(list, newIndex + 1, prevIndex + 1).forEach((item) => {
+      animate(item as HTMLElement, 'down')
+    })
+    animate(item as HTMLElement, 'up')
+  }
+}
+
 export default function Panel(props: PanelProps) {
   const draggableList = useRef<HTMLDivElement>(null)
 
-  const { chosenItem, chosenItemIndex, chosenItemParent } = useContext(DragContext)
+  const { chosenItem, chosenItemParent } = useContext(DragContext)
 
-  const getItemIndex = (item: HTMLElement) =>
-    Array.from(draggableList.current?.children ?? []).indexOf(item)
-
-  const getItemsSlice = (list: HTMLDivElement, start: number, end?: number) =>
-    Array.from(list.children).slice(start, end ?? list.childElementCount)
+  const getItemIndex = (item: HTMLElement) => {
+    // const thisPanelItems = Array.from(draggableList.current!.children)
+    // return thisPanelItems.includes(item)
+    //   ? thisPanelItems.indexOf(item)
+    //   : Array.from(chosenItemParent.current!.children).indexOf(item)
+    return (
+      (Array.from(draggableList.current!.children).indexOf(item) + 1 ||
+        Array.from(chosenItemParent.current!.children).indexOf(item) + 1) - 1
+    )
+  }
 
   const dragStart = (e: React.DragEvent<HTMLDivElement>) => {
     chosenItem.current = e.currentTarget
-    chosenItemIndex.current = getItemIndex(e.currentTarget)
     chosenItemParent.current = draggableList.current
-    console.log('dragStart', chosenItemIndex.current)
+    console.log('dragStart', getItemIndex(e.currentTarget))
   }
 
   const dragEnter = (e: React.DragEvent<HTMLDivElement>) => {
     const target = e.currentTarget as HTMLElement
-    if (chosenItem.current === target) return
 
-    for (const anim of target?.getAnimations() ?? []) {
+    if (target === chosenItem.current) return
+
+    for (const anim of target!.getAnimations()) {
       if (anim.playState === 'running') return
     }
 
     if (chosenItemParent.current !== draggableList.current) {
       console.log('dragEnter', 'different parent')
+
+      const prevIndex = getItemIndex(chosenItem.current!)
       target.before(chosenItem.current!)
 
-      getItemsSlice(chosenItemParent.current!, chosenItemIndex.current!).forEach((item) => {
+      getItemsSlice(chosenItemParent.current!, prevIndex).forEach((item) => {
         animate(item as HTMLElement, 'up')
       })
 
-      chosenItemIndex.current = getItemIndex(chosenItem.current!)
+      const newIndex = getItemIndex(chosenItem.current!)
       chosenItemParent.current = draggableList.current
-      getItemsSlice(draggableList.current!, chosenItemIndex.current + 1).forEach((item) => {
+
+      getItemsSlice(draggableList.current!, newIndex + 1).forEach((item) => {
         animate(item as HTMLElement, 'down')
       })
 
       return
     }
 
-    const prevIndex = chosenItemIndex.current!
+    const prevIndex = getItemIndex(chosenItem.current!)
+    const newIndex = getItemIndex(target)
 
-    if (prevIndex < getItemIndex(target)) {
-      target.after(chosenItem.current!)
-      chosenItemIndex.current = getItemIndex(chosenItem.current!)
+    moveItems(draggableList.current!, prevIndex, newIndex)
 
-      console.log('dragEnter', prevIndex, chosenItemIndex.current)
-
-      getItemsSlice(draggableList.current!, prevIndex, chosenItemIndex.current).forEach((item) => {
-        animate(item as HTMLElement, 'up')
-      })
-      animate(chosenItem.current!, 'down')
-    } else {
-      target.before(chosenItem.current!)
-      chosenItemIndex.current = getItemIndex(chosenItem.current!)
-
-      console.log('dragEnter', prevIndex, chosenItemIndex.current)
-
-      getItemsSlice(draggableList.current!, chosenItemIndex.current + 1, prevIndex + 1).forEach(
-        (item) => {
-          animate(item as HTMLElement, 'down')
-        },
-      )
-      animate(chosenItem.current!, 'up')
-    }
+    console.log('dragEnter', prevIndex, newIndex)
   }
 
   return (
